867f7a6376830658db29e866e249166877563e2d
James <jameshamby2@icloud.com>
2024-08-29 21:16:38 -0500

    Testing, I having a hard time getting github to connect to right stuff

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print("99")
-    else:
-        print("{0:0=2d}, ".format(i), end='')
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
