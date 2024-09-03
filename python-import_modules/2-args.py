2dffef1e17d80950b5a736eff41a5ca880bd52f4
James <jameshamby2@icloud.com>
2024-09-02 19:49:02 -0500

    testing

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-import sys
-
-def main():
-    arg_count = len(sys.argv) - 1
-    argument = "argument" if arg_count == 1 else "arguments"
-    if arg_count == 0:
-        print("0 arguments.")
-    else:
-        print(f"{arg_count} {argument}:")
-        for index, value in enumerate(sys.argv[1:], start=1):
-            print(f"{index}: {value}")
 if __name__ == "__main__":
-    main()
+    import sys
+
+    c = len(sys.argv) - 1
+    if c == 0:
+        print("0 arguments.")
+    elif c == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(c))
+
+    for i in range(c):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
