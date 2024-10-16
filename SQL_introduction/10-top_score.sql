04515d416893ae0a649d148ec92ab4a5d72da080
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-10-15 22:53:49 -0500

    rest of this one

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of a table of a database
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- script that lists all records
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
