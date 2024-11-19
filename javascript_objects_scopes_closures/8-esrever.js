fbde29cca5e5eaabb363b8c2a7bc2d82d017e32f
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-11-19 23:39:47 +0000

    finishing up

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+    const reversed = [];
+    for (let i = 0; i < list.length; i++) {
+      reversed[i] = list[list.length - i - 1];
+    }
+    return reversed;
+  };
+  
